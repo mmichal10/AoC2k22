100687aac4d7d0525e7165f73ded6ebb3e81adaf
Michal Mielewczyk <michal.mielewczyk@intel.com>
2022-12-01 19:33:13 +0100

    --- Day 1: Calorie Counting ---
    Second part of task 1
    
    Signed-off-by: Michal Mielewczyk <michal.mielewczyk10@gmail.com>

--- 1.py
+++ 1.py
@@ -1,12 +1,18 @@
 #!/usr/bin/python3.10
 
 with open('1.input') as f:
-    calories_lists_raw = f.read().split("\n\n")
-    calories_str_list = [cal.split() for cal in calories_lists_raw]
+    calories_lists_raw = f.read()
 
-    summed_calories = [
-        sum([int(s) for s in calories_str])
-        for calories_str in calories_str_list
-    ]
+calories_lists_raw = calories_lists_raw.split("\n\n")
 
-    print(max(summed_calories))
+calories_str_list = [cal.split() for cal in calories_lists_raw]
+
+summed_calories = [
+    sum([int(s) for s in calories_str])
+    for calories_str in calories_str_list
+]
+
+summed_calories.sort(reverse=True)
+
+print(f"Max value {summed_calories[0]}")
+print(f"Sum of top 3 {sum(summed_calories[:3])}")
