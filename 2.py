9c2509413b8ecc66aec508f8f132df342ca9834f
Michal Mielewczyk <michal.mielewczyk@intel.com>
2022-12-02 17:05:14 +0100

    --- Day 2: Rock Paper Scissors ---
    Part 2
    
    Signed-off-by: Michal Mielewczyk <michal.mielewczyk10@gmail.com>

--- 2.py
+++ 2.py
@@ -21,6 +21,15 @@ class Shape(Enum):
     Y = PAPER
     Z = SCISSORS
 
+class ExpectedResult(Enum):
+    LOST = 1
+    DRAW = 2
+    WIN = 3
+
+    X = LOST
+    Y = DRAW
+    Z = WIN
+
 def result_part1(opponent, my):
     opponent_shape = Shape[opponent]
     my_shape = Shape[my]
@@ -39,6 +48,37 @@ def result_part1(opponent, my):
 
     return LOOSING_BONUS + my_shape.value
 
+def _get_beating_shape_value(shape):
+    match shape:
+        case Shape.ROCK:
+            return Shape.PAPER.value
+        case Shape.PAPER:
+            return Shape.SCISSORS.value
+        case Shape.SCISSORS:
+            return Shape.ROCK.value
+
+def _get_beaten_shape_value(shape):
+    match shape:
+        case Shape.ROCK:
+            return Shape.SCISSORS.value
+        case Shape.PAPER:
+            return Shape.ROCK.value
+        case Shape.SCISSORS:
+            return Shape.PAPER.value
+
+def result_part2(opponent, expected_result):
+    opponent_shape = Shape[opponent]
+    expected_result = ExpectedResult[expected_result]
+
+    if expected_result == ExpectedResult.DRAW:
+        return DRAW_BONUS + opponent_shape.value
+
+    if expected_result == ExpectedResult.WIN:
+        return WINNING_BONUS + _get_beating_shape_value(opponent_shape)
+
+    if expected_result == ExpectedResult.LOST:
+        return LOOSING_BONUS + _get_beaten_shape_value(opponent_shape)
+
 with open('2.input') as f:
     duels = f.read()
 
@@ -46,6 +86,8 @@ duel_list = duels.split('\n')
 
 duel_list.pop() # The last element is empty
 
-results = [result_part1(*d.split()) for d in duel_list]
+results1 = [result_part1(*d.split()) for d in duel_list]
+results2 = [result_part2(*d.split()) for d in duel_list]
 
-print(f"Result: {sum(results)}")
+print(f"Result 1: {sum(results1)}")
+print(f"Result 2: {sum(results2)}")
